68e3ca6f6b001b52578f3a5b187297bf7a6d030b
q2l3ntk <zurskij@gmail.com>
2024-10-28 17:09:37 +0300

    Import kotlinx serialization and lombok configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.serialization") version "2.0.21"
+	kotlin("plugin.lombok") version "2.0.21"
+	id("io.freefair.lombok") version "8.10"
 	id("org.springframework.boot") version "3.2.11"
 	id("io.spring.dependency-management") version "1.1.6"
 }
@@ -22,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -31,6 +35,10 @@ kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
+}
+
+kotlinLombok {
+	lombokConfigurationFile(file("lombok.config"))
 }
 
 tasks.withType<Test> {
