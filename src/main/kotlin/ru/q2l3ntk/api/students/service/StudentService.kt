192509fc9b624e1bd977b8e98c9068dac79107e3
q2l3ntk <zurskij@gmail.com>
2024-10-28 18:01:58 +0300

    Set implemented interface

--- src/main/kotlin/ru/q2l3ntk/api/students/service/StudentService.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/service/StudentService.kt
@@ -1,15 +1,7 @@
 package ru.q2l3ntk.api.students.service
 
-import org.springframework.stereotype.Service
 import ru.q2l3ntk.api.students.model.Student
 
-@Service
-class StudentService {
-    fun findAllStudents(): List<Student>{
-        return listOf(
-            Student().setFirstName("John").setEmail("john.doe@gmail.com").setAge(41),
-            Student().setFirstName("Freddy").setEmail("freddy.doe@gmail.com").setAge(38),
-            Student().setFirstName("Anna").setEmail("anna123@yandex.ru").setAge(25),
-        )
-    }
+interface StudentService {
+    fun findAllStudents(): List<Student>
 }
