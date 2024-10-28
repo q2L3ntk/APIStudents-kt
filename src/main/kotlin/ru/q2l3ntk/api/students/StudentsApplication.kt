434fc8079563ae30212d447aae8da204f5d53f40
q2l3ntk <zurskij@gmail.com>
2024-10-28 17:00:19 +0300

    Students application (API)

---
+++ src/main/kotlin/ru/q2l3ntk/api/students/StudentsApplication.kt
@@ -0,0 +1,11 @@
+package ru.q2l3ntk.api.students
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentsApplication>(*args)
+}
