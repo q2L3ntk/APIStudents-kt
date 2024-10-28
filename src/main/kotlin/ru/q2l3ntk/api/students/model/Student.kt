57eca186b226b450e0e1b0e1a965d19226292178
q2l3ntk <zurskij@gmail.com>
2024-10-28 17:39:57 +0300

    Student model

---
+++ src/main/kotlin/ru/q2l3ntk/api/students/model/Student.kt
@@ -0,0 +1,44 @@
+package ru.q2l3ntk.api.students.model
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import lombok.Builder
+import lombok.Data
+import java.time.LocalDate
+
+@Data
+@Builder
+@Serializable
+class Student {
+    private var firstName: String? = null
+    private var lastName: String? = null
+    @Contextual
+    private var dateOfBirth: LocalDate? = null
+    private var email: String? = null
+    private var age: Int? = null
+
+    fun setFirstName(firstName: String?): Student {
+        this.firstName = firstName
+        return this
+    }
+
+    fun setLastName(lastName: String?): Student {
+        this.lastName = lastName
+        return this
+    }
+
+    fun setDateOfBirth(dateOfBirth: LocalDate?): Student {
+        this.dateOfBirth = dateOfBirth
+        return this
+    }
+
+    fun setEmail(email: String?): Student {
+        this.email = email
+        return this
+    }
+
+    fun setAge(age: Int?): Student {
+        this.age = age
+        return this
+    }
+}
