dfab389fbd302ba4b16aad21dbab5e7f7eb43d48
q2l3ntk <zurskij@gmail.com>
2024-11-03 23:17:36 +0300

    Override functions and using autowired

--- src/main/kotlin/ru/q2l3ntk/api/students/service/impl/InMemoryStudentServiceImpl.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/service/impl/InMemoryStudentServiceImpl.kt
@@ -1,28 +1,33 @@
 package ru.q2l3ntk.api.students.service.impl
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import ru.q2l3ntk.api.students.model.Student
+import ru.q2l3ntk.api.students.repository.InMemoryStudentDAO
 import ru.q2l3ntk.api.students.service.StudentService
 
 @Service
 class InMemoryStudentServiceImpl: StudentService {
+    @Autowired
+    private lateinit var repository: InMemoryStudentDAO
+
     override fun findAllStudents(): List<Student> {
-        TODO("Not yet implemented")
+        return repository.findAllStudents()
     }
 
     override fun saveStudent(student: Student): Student {
-        TODO("Not yet implemented")
+        return repository.saveStudent(student)
     }
 
-    override fun findByEmail(email: String?): Student {
-        TODO("Not yet implemented")
+    override suspend fun findByEmail(email: String): Student? {
+        return repository.findByEmail(email)
     }
 
-    override fun updateStudent(student: Student): Student {
-        TODO("Not yet implemented")
+    override fun updateStudent(student: Student): Student? {
+        return repository.updateStudent(student)
     }
 
-    override fun deleteStudent(email: String?) {
-        TODO("Not yet implemented")
+    override suspend fun deleteStudent(email: String) {
+        return repository.deleteStudent(email)
     }
 }
