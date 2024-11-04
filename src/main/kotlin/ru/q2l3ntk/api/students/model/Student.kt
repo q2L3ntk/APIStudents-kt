c6552481467e6ae1d588428ca3155d4e4b0fa0f5
q2l3ntk <zurskij@gmail.com>
2024-11-04 23:20:33 +0300

    Student model fix

--- src/main/kotlin/ru/q2l3ntk/api/students/model/Student.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/model/Student.kt
@@ -13,26 +13,6 @@ class Student {
     private var email: String? = null
     private var age: Int? = null
 
-    fun setFirstName(firstName: String?): Student {
-        this.firstName = firstName
-        return this
-    }
-
-    fun setLastName(lastName: String?): Student {
-        this.lastName = lastName
-        return this
-    }
-
-    fun setDateOfBirth(dateOfBirth: LocalDate?): Student {
-        this.dateOfBirth = dateOfBirth
-        return this
-    }
-
-    fun setEmail(email: String?): Student {
-        this.email = email
-        return this
-    }
-
     fun getFirstName(): String? {
         return this.firstName
     }
@@ -51,10 +31,5 @@ class Student {
 
     fun getAge(): Int? {
         return this.age
-    }
-
-    fun setAge(age: Int?): Student {
-        this.age = age
-        return this
     }
 }
