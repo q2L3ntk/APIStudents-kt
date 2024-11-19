db63e59246e235b129156ac6142c1b6a91ee0b9d
q2l3ntk <zurskij@gmail.com>
2024-11-19 16:14:08 +0300

    Delete api filter, add method security

--- src/main/kotlin/ru/q2l3ntk/api/students/config/SecurityConfig.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/config/SecurityConfig.kt
@@ -2,7 +2,7 @@ package ru.q2l3ntk.api.students.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -16,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig {
     @Bean
     fun userDetailsService(encoder: PasswordEncoder): UserDetailsService {
@@ -23,23 +24,6 @@ class SecurityConfig {
         val user: UserDetails = User.builder().username("user").password(encoder.encode("user")).roles("USER").build()
 
         return InMemoryUserDetailsManager(admin, user)
-    }
-
-    @Order(1)
-    @Bean
-    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http {
-            csrf { disable() }
-            securityMatcher(
-                "api/v1/students/save_student",
-                "api/v1/students/update_student",
-                "api/v1/students/delete_student"
-            )
-            authorizeRequests { authorize(anyRequest, hasRole("ADMIN")) }
-            httpBasic { }
-        }
-
-        return http.build()
     }
 
     @Bean
