14a6669e3a7ef5e11e8789a27971504be28f564e
q2l3ntk <zurskij@gmail.com>
2024-11-19 16:15:18 +0300

    Sharing access at a point between roles

--- src/main/kotlin/ru/q2l3ntk/api/students/controller/StudentController.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/controller/StudentController.kt
@@ -2,6 +2,7 @@ package ru.q2l3ntk.api.students.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 import ru.q2l3ntk.api.students.model.Student
@@ -18,6 +19,7 @@ class StudentController {
     fun findAllStudents(): List<Student> = service.findAllStudents()
 
     @PostMapping("save_student")
+    @PreAuthorize("hasRole('ADMIN')")
     fun saveStudent(@RequestBody student: Student): String {
         service.saveStudent(student)
         return "Student successfully saved"
@@ -27,9 +29,11 @@ class StudentController {
     fun findByEmail(@PathVariable email: String): Student? = service.findByEmail(email)
 
     @PutMapping("update_student")
+    @PreAuthorize("hasRole('ADMIN')")
     fun updateStudent(@RequestBody student: Student): Student? = service.updateStudent(student)
 
     @DeleteMapping("delete_student/{email}")
+    @PreAuthorize("hasRole('ADMIN')")
     @Transactional
     fun deleteStudent(@PathVariable email: String) { service.deleteStudent(email) }
 }
