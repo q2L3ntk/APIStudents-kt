da7af22fdac33a0a837c75aec02ee31e8699ca1c
q2l3ntk <zurskij@gmail.com>
2024-11-25 21:17:01 +0300

    User repository fix

--- src/main/kotlin/ru/q2l3ntk/api/students/repository/UserRepository.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/repository/UserRepository.kt
@@ -1,10 +1,8 @@
 package ru.q2l3ntk.api.students.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import ru.q2l3ntk.api.students.model.CommonUser
 
-@Repository
 interface UserRepository: JpaRepository<CommonUser, Long> {
-    fun findByUsername(username: String): CommonUser?
+    fun findByName(username: String): CommonUser?
 }
