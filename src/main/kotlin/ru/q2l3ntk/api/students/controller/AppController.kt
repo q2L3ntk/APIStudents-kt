7db2540bc7237aed9bfe7f5baa1920c6053343f3
q2l3ntk <zurskij@gmail.com>
2024-11-25 21:17:44 +0300

    Add user function released

--- src/main/kotlin/ru/q2l3ntk/api/students/controller/AppController.kt
+++ src/main/kotlin/ru/q2l3ntk/api/students/controller/AppController.kt
@@ -5,15 +5,20 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
+import ru.q2l3ntk.api.students.model.CommonUser
 import ru.q2l3ntk.api.students.model.Student
-import ru.q2l3ntk.api.students.service.StudentService
+import ru.q2l3ntk.api.students.service.AppService
+import ru.q2l3ntk.api.students.service.CommonUserDetailsService
 
 @RestController
 @RequestMapping("/api/v1/students")
-class StudentController {
-    @Qualifier("studentServiceImpl")
+class AppController {
+    @Qualifier("appServiceImpl")
     @Autowired //Outdated
-    private lateinit var service: StudentService
+    private lateinit var service: AppService
+
+    @Autowired
+    private lateinit var userService: CommonUserDetailsService
 
     @GetMapping
     fun findAllStudents(): List<Student> = service.findAllStudents()
@@ -36,4 +41,10 @@ class StudentController {
     @PreAuthorize("hasRole('ADMIN')")
     @Transactional
     fun deleteStudent(@PathVariable email: String) { service.deleteStudent(email) }
+
+    @PostMapping("/new-user")
+    fun addUser(@RequestBody user: CommonUser): String {
+        userService.addUser(user)
+        return "New user successfully added"
+    }
 }
